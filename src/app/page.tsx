a6af2f47e5bb025af40a99b8bae940b88e077789
rjkush17 <rishabhkushwahrj@gmail.com>
2024-10-08 20:54:11 +0530

    added: shadcn.ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
       <p className="underline">links next app</p>
+      <Button>Click me</Button>
+
     </div>
   );
 }
